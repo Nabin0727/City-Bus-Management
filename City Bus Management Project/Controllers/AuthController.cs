37be713bdc8a77026baca914020821e2f1442051
Nabin Lopchan <lopchannabeen138@gmail.com>
2022-06-13 22:00:23 +0545

    initial commit

---
+++ City Bus Management Project/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace City_Bus_Management_Project.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Signup()
+        {
+            return View();
+        }
+        public ViewResult Login()
+        {
+            return View();
+            
+        }
+    }
+}
---
+++ City Bus Management Project/Controllers/DashboardController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace City_Bus_Management_Project.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Dashboard()
+        {
+            return View();
+        }
+
+        public ViewResult Add()
+        {
+            return View();
+        }
+    }
+}
